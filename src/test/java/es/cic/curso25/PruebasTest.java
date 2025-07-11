0880d8d83319eeee823229fe303e88226a5e17a7
LuisAcereda <no@nomail.no>
2025-07-11 10:20:22 +0200

    Estable explicacion de herencias

--- src/test/java/es/cic/curso25/PruebasTest.java
+++ src/test/java/es/cic/curso25/PruebasTest.java
@@ -1,14 +1,16 @@
 package es.cic.curso25;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class PruebasTest {
-
+    @Disabled
     @Test
     public void acelerarRemotamente(Coche coche, int incremento){
         coche.acelerar(incremento);
     }
 
+    @Disabled
     @Test
     public void acelerarRemotamente(Camion camion, int incremento){
         camion.acelerar(incremento);
